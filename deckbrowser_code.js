fa56c3b38b6b8d883c80938f55ba6898a872b02f
BaSsim <bassim.bakkali.gazuani@gmail.com>
2023-11-14 15:43:38 +0100

    Fix scroll glitch whn deck is out of view initially + Handle S and Enter the same

--- deckbrowser_code.js
+++ deckbrowser_code.js
@@ -2,32 +2,34 @@ document.addEventListener('keydown', function (event) {
     var currentDeck = document.querySelector('tr.deck.current');
     if (!currentDeck) return;
 
+    currentDeck.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+
     switch (event.code) {
-        case 'ArrowUp':            
+        case 'ArrowUp':
         case 'ArrowDown':
             event.preventDefault();
-			
-			let direction = event.code === 'ArrowUp' ? "Up" : "Down";
+
+            let direction = event.code === 'ArrowUp' ? "Up" : "Down";
 
             var decks = Array.from(document.querySelectorAll('tr.deck'));
             var currentIndex = decks.indexOf(currentDeck);
             var nextIndex = currentIndex + (direction === 'Up' ? -1 : 1);
 
             // Boundary conditions
-            if (nextIndex < 0 || nextIndex >= decks.length) { window.scrollBy({ top: direction === 'Up' ? -10 : 10, behavior: 'smooth' }); return };
+            if (nextIndex < 0 || nextIndex >= decks.length) { window.scrollBy({ top: direction === 'Up' ? -10 : 10, behavior: 'smooth' }); return; };
 
             // Remove 'current' class from the current deck and add it to the next one
             currentDeck.classList.remove('current');
             var nextDeck = decks[nextIndex];
             nextDeck.classList.add('current');
-			
-			var scrollToDeck = decks[nextIndex + (direction === 'Up' ? -3 : 3)];
+
+            var scrollToDeck = decks[nextIndex + (direction === 'Up' ? -3 : 3)];
 
             // Scroll into view if out of viewport
             nextDeck.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
-			
-			// Scroll ahead to show neighbors too
-			scrollToDeck?.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+
+            // Scroll ahead to show neighbors too
+            scrollToDeck?.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
             break;
         case 'ArrowRight':
         case 'ArrowLeft':
@@ -65,12 +67,6 @@ document.addEventListener('keydown', function (event) {
             pycmd('openAddDialog');
             break;
         case 'KeyS':
-            // Get the id of the current deck
-            var deckId = currentDeck.id;
-            // Check if a deckId was found and construct the pycmd
-            if (deckId) {
-                pycmd("select:" + deckId);
-            }
         case 'Enter':
             // Simulate click on the deck link within the selected deck
             var deckLink = currentDeck.querySelector('td.decktd > a.deck');
